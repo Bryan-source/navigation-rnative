9556df0f527017059e23b3bf046e42d2c54d9398
bryan-source <bescobar34@yahoo.com>
2022-05-06 11:07:06 -0500

    Add some styles and vreate MenuScreen

--- DetailScreen.js
+++ DetailScreen.js
@@ -1,25 +1,66 @@
 import React from 'react';
 import {StyleSheet, Button, View, Text} from 'react-native';
 
-function DetailScreen({navigation}) {
+function DetailScreen({route, navigation}) {
+
+    const {id, description} = route.params;
+
     return (
-        <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'green'}}>
+        <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#66bb6a'}}>
             <Text>Details Screen</Text>
+            <Text>Screen: {id}</Text>
+            <Text>Description: {description}</Text>
 
             <View style={styles.containerButton}>
                 <Button 
+                    color="#424242"
+                    title="Tarea 1"
+                    onPress={() => navigation.push('Details', {
+                        id: 1,
+                        description: "Ir de compras"
+                    })}
+                />
+                <Button 
+                    color="#424242"
+                    title="Tarea 2"
+                    onPress={() => navigation.push('Details', {
+                        id: 2,
+                        description: "Jugar"
+                    })}
+                />
+                <Button 
+                    color="#424242"
+                    title="Tarea 3"
+                    onPress={() => navigation.push('Details',  {
+                        id: 3,
+                        description: "Asistir a clase"
+                    })}
+                />
+            </View>
+
+            <View style={styles.containerButton}>
+                <Button 
+                    color="#424242"
                     title="Atrás"
                     onPress={() => navigation.goBack()}
                 />
                 <Button 
+                    color="#424242"
                     title="Home"
                     onPress={() => navigation.navigate('Home')}
                 />
                 <Button 
+                    color="#424242"
                     title="Ir a Detalle"
                     onPress={() => navigation.push('Details')}
                 />
             </View>
+
+            <Button
+                color="#424242"
+                title="Inicio"
+                onPress={() => navigation.popToTop()}
+            />
             
         </View>
     );
@@ -30,7 +71,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center', 
-        backgroundColor: 'gray',
+        backgroundColor: '#98ee99',
 
         margin: 20,
         padding: 5,
--- HomeScreen.js
+++ HomeScreen.js
@@ -8,7 +8,10 @@ function HomeScreen({navigation}) {
             <Text style={styles.text}>Home</Text>
             <Button 
                 title="Ir a Detalle"
-                onPress={() => navigation.navigate('Details')}
+                onPress={() => navigation.navigate('Details', {
+                    id: 1, 
+                    description: 'Aquí va la descripción',
+                } )}
             />
             
         </View>
